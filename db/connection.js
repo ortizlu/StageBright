ca93756f2238df52599f4a1e608c6816ada433f3
Luis Ortiz <ortizlu@icloud.com>
2018-10-05 13:12:43 -0400

    add mongo

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/stagebright')
+if (process.env.NODE_ENV == 'production') {
+  mongoose.connect(process.env.MLAB_URL)
+} else {
+  mongoose.connect('mongodb://localhost/stagebright')
+}
 mongoose.Promise = Promise
 module.exports = mongoose
