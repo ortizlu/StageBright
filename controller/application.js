a63e708ee2a7d8a3a7dbcc8afe0d8a12fe1726f4
Luis Ortiz <ortizlu@icloud.com>
2018-10-05 13:18:35 -0400

    bug check

--- controller/application.js
+++ controller/application.js
@@ -3,12 +3,16 @@ const Post = require('../models/post')
 // router.get('/', applicationController.index)
 module.exports = {
   index: (req, res) => {
-    Post.find({})
-      .sort({ createdAt: -1 })
-      .limit(10)
-      .populate('author')
-      .then(posts => {
-        res.render('app/index', { posts })
-      })
+    if (req.params.id !== 'favicon.ico') {
+      Post.find({})
+        .sort({ createdAt: -1 })
+        .limit(10)
+        .populate('author')
+        .then(posts => {
+          res.render('app/index', { posts })
+        })
+    } else {
+      res.send('hello world')
+    }
   }
 }
